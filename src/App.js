e839edc307a8aa23dc9687882d2775a978a3ad24
andrei moskalev <moskalev.web.dev@gmail.com>
2024-11-30 19:12:48 +0300

    Commmmmit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from "./logo.svg";
 import styles from "./app.module.css";
 
 export const App = () => {
 	return (
 		<div className={styles.App}>
-			<div>Привет</div>
+			<div></div>
 		</div>
 	);
 };
