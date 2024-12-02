0e9ad8c901ef44d9d5b79ec1d2becc9fcd18bd17
andrei moskalev <moskalev.web.dev@gmail.com>
2024-12-02 21:59:44 +0300

    8. Классовые компоненты. Создание. Вывод состояния

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
+import { Component, useState, useEffect } from "react";
 import styles from "./app.module.css";
 
-export const App = () => {
+export const App = ({ message }) => {
+	const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+	useEffect(() => {
+		console.log(message);
+
+		const updateScreenWidth = () => setScreenWidth(window.innerWidth);
+
+		window.addEventListener("resize", updateScreenWidth);
+
+		return () => window.removeEventListener("resize", updateScreenWidth);
+	}, []);
+
 	return (
 		<div className={styles.App}>
-			<div></div>
+			{message}: {screenWidth}
 		</div>
 	);
 };
+
+export class OldApp extends Component {
+	// state = window.innerWidth; Как вариант, можно объявить состояние здесь
+
+	constructor() {
+		super();
+		this.state = {
+			screenWidth: window.innerWidth,
+		}; // Но обычно объявляют в конструкторе
+	}
+
+	render() {
+		return (
+			<div className={styles.App}>
+				{/*{message}*/}: {this.state.screenWidth}
+			</div>
+		);
+	}
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import { App } from './App';
+import React, { Component } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { App, OldApp } from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<>
+		<App message="Функциональный компонент" />
+		<OldApp message="Классовый компонент" />
+	</>,
 );
